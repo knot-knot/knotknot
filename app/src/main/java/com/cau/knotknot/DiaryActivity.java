a46eca417fc2e53bc0a2a63abc4270eb34777a85
wooji9 <woojungin99@naver.com>
2021-05-01 01:46:02 +0900

    main에서 로그인 클릭->diary에서 +클릭->write

--- app/src/main/java/com/cau/knotknot/DiaryActivity.java
+++ app/src/main/java/com/cau/knotknot/DiaryActivity.java
@@ -2,7 +2,11 @@ package com.cau.knotknot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -14,6 +18,7 @@ public class DiaryActivity extends AppCompatActivity {
     long mNow;
     Date mDate;
     SimpleDateFormat mFormat = new SimpleDateFormat("MM월 dd일 (E)");
+    ImageButton add;
 
     private String getTime(){
         mNow = System.currentTimeMillis();
@@ -26,8 +31,17 @@ public class DiaryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
 
+        add = (ImageButton)findViewById(R.id.diary_add);
+        date = (TextView)findViewById(R.id.date);
+        date.setText(getTime());//수정할것!-->캘린더 선택한 날짜로 바꿔야함
 
-        date = findViewById(R.id.date);
-        date.setText(getTime());//캘린더 선택한 날짜로 바꿔야함
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    Intent intent = new Intent(getApplicationContext(),WriteActivity.class);
+                    startActivity(intent);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/cau/knotknot/MainActivity.java
+++ app/src/main/java/com/cau/knotknot/MainActivity.java
@@ -2,13 +2,57 @@ package com.cau.knotknot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText id, pw;
+    Button login, join;
+    String s_id, s_pw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*id = (EditText)findViewById(R.id.main_id);
+
+        if ( id.getText().toString().length() == 0 ) {
+            //공백일 때 에러 메시지
+        } else {
+            s_id = id.getText().toString();
+        }
+
+        pw = (EditText)findViewById(R.id.main_pw);
+
+        if ( pw.getText().toString().length() == 0 ) {
+            //공백일 때 에러 메시지
+        } else {
+            s_pw = pw.getText().toString();
+        }*/
+
+        login = (Button)findViewById(R.id.main_login);
+        join = (Button)findViewById(R.id.main_join);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Log.d("maintodiary","s_id : "+s_id+" s_pw:"+s_pw);
+                //if(s_id.equals("a")&&s_pw.equals("a")) {
+                //    Log.d("maintodiary", "equal판별은 함");
+                    Intent intent = new Intent(getApplicationContext(), DiaryActivity.class);
+                    intent.putExtra("message", "DiaryActivity");
+                    startActivity(intent);
+                //}
+                //else{
+                //    Log.d("maintodiary", "equal아님");
+                //}
+            }
+        });
+
     }
 }
