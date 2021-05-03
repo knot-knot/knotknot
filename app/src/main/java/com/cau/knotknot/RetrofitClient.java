d7543cb0b126339cb20ab1139d14b404bc62e534
sage-cau <dan06052@naver.com>
2021-05-03 18:32:46 +0900

    Retrofit 객체 생성 (싱글톤 패턴)

---
+++ app/src/main/java/com/cau/knotknot/RetrofitClient.java
@@ -0,0 +1,30 @@
+package com.cau.knotknot;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static RetrofitClient instance = null;
+    private static RetrofitInterface retrofitInterface;
+    private static String baseUrl = "127.0.0.1:3000";
+
+    private RetrofitClient() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        retrofitInterface = retrofit.create(RetrofitInterface.class);
+    }
+
+    // 싱글톤 패턴
+    public static RetrofitClient getInstance() {
+        if (instance == null) {
+            instance = new RetrofitClient();
+        }
+        return instance;
+    }
+
+    public static RetrofitInterface getRetrofitInterface() {
+        return retrofitInterface;
+    }
+}
