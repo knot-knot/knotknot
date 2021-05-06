7de2070c9e76fadba03548d9280c5d53a6c3136c
sage-cau <dan06052@naver.com>
2021-05-06 23:23:21 +0900

    comments api

---
+++ app/src/main/java/com/cau/knotknot/Comments.java
@@ -0,0 +1,87 @@
+package com.cau.knotknot;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Comments {
+    @SerializedName("comments_id")
+    @Expose
+    private Integer commentsId;
+    @SerializedName("content")
+    @Expose
+    private String content;
+    @SerializedName("createdAt")
+    @Expose
+    private String createdAt;
+    @SerializedName("user.nickname")
+    @Expose
+    private String userNickname;
+
+    public Comments(Integer commentsId, String content, String createdAt, String userNickname) {
+        super();
+        this.commentsId = commentsId;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.userNickname = userNickname;
+    }
+
+    public Integer getCommentsId() {
+        return commentsId;
+    }
+
+    public void setCommentsId(Integer commentsId) {
+        this.commentsId = commentsId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUserNickname() {
+        return userNickname;
+    }
+
+    public void setUserNickname(String userNickname) {
+        this.userNickname = userNickname;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Diary.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("commentsId");
+        sb.append('=');
+        sb.append(((this.commentsId == null) ? "<null>" : this.commentsId));
+        sb.append(',');
+        sb.append("content");
+        sb.append('=');
+        sb.append(((this.content == null) ? "<null>" : this.content));
+        sb.append(',');
+        sb.append("createdAt");
+        sb.append('=');
+        sb.append(((this.createdAt == null) ? "<null>" : this.createdAt));
+        sb.append(',');
+        sb.append("userNickname");
+        sb.append('=');
+        sb.append(((this.userNickname == null) ? "<null>" : this.userNickname));
+        sb.append(',');
+        if (sb.charAt((sb.length() - 1)) == ',') {
+            sb.setCharAt((sb.length() - 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+}
--- app/src/main/java/com/cau/knotknot/RetrofitInterface.java
+++ app/src/main/java/com/cau/knotknot/RetrofitInterface.java
@@ -4,20 +4,65 @@ import java.util.List;
 import java.util.Map;
 
 import retrofit2.Call;
+import retrofit2.http.DELETE;
 import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface RetrofitInterface {
 
+    //로그인
+    @POST("login")
+    @FormUrlEncoded
+    Call<Void> login(@FieldMap Map<String, Object> fields);
+
+    //회원가입
+    @POST("register")
+    @FormUrlEncoded
+    Call<Void> register(@FieldMap Map<String, Object> fields);
+
+    //로그아웃
+    @GET("logout")
+    Call<Void> logout();
+
+    //일기 불러오기
     @GET("diary/{username}")
     Call<List<Diary>> getDiary(@Path("username") String username, @Query("date") String date);
 
+    //일기 작성
     @POST("diary")
     @FormUrlEncoded
     Call<Void> createDiary(@FieldMap Map<String, Object> fields);
+
+    //일기 수정
+    @PUT("diary/{diaryId}")
+    @FormUrlEncoded
+    Call<Void> updateDiary(@Path("diaryId") int diaryId, @FieldMap Map<String, Object> fields);
+
+    //일기 삭제
+    @DELETE("diary/{diaryId}")
+    Call<Void> deleteDiary(@Path("diaryId") int diaryId);
+
+    //코멘트 불러오기 (일기 1개 당)
+    @GET("comments/{diaryId}")
+    Call<List<Comments>> getComments(@Path("diaryId") int diaryId);
+
+    //코멘트 작성
+    @POST("comments/{diaryId}")
+    @FormUrlEncoded
+    Call<Void> createComments(@FieldMap Map<String, Object> fields);
+
+    //코멘트 수정
+    @PUT("comments/{commentsId}")
+    @FormUrlEncoded
+    Call<Void> updateComments(@Path("commentsId") int commentsId, @FieldMap Map<String, Object> fields);
+
+    //코멘트 삭제
+    @DELETE("comments/{commentsId}")
+    Call<Void> deleteComments(@Path("commentsId") int commentsId);
 
 }
