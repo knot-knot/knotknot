4b376e7a5f8b4a252177e17ca945e77596892f18
sage-cau <dan06052@naver.com>
2021-05-25 16:56:21 +0900

    로그인 후에는 요청 헤더에 jwt 토큰을 intercept

--- app/src/main/java/com/cau/knotknot/RetrofitClient.java
+++ app/src/main/java/com/cau/knotknot/RetrofitClient.java
@@ -1,18 +1,25 @@
 package com.cau.knotknot;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static RetrofitClient instance = null;
     private static RetrofitInterface retrofitInterface;
-    private static String baseUrl = "http://10.0.2.2:3000";
+    private static final String baseUrl = "http://10.0.2.2:3000";
+
+    private static Retrofit.Builder builder = new Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create());
 
     private RetrofitClient() {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+        Retrofit retrofit = builder.build();
         retrofitInterface = retrofit.create(RetrofitInterface.class);
     }
 
@@ -24,7 +31,27 @@ public class RetrofitClient {
         return instance;
     }
 
+    // 로그인 시에만 token 을 헤더에 주입시키는 instance 다시 생성
+    public static RetrofitClient getInstanceWithToken(String token) {
+
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request newRequest = chain.request().newBuilder()
+                        .addHeader("Authorization", "Bearer " + token)
+                        .build();
+                return chain.proceed(newRequest);
+            }
+        }).build();
+        builder.client(client);
+
+        instance = new RetrofitClient();
+        return instance;
+    }
+
     public static RetrofitInterface getRetrofitInterface() {
         return retrofitInterface;
     }
 }
+
+
